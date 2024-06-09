db8925edfc5d870751f4e831a42e4dad107c15fd
Faizalayezal <faizallingadiya2000@gmail.com>
2024-06-08 23:56:34 -0700

    firestCommit

--- app/src/main/java/com/example/faizaltask1/MainActivity.kt
+++ app/src/main/java/com/example/faizaltask1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             FaizalTask1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Faizal",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
